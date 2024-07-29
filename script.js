5e7a3498ab1bb17e18f2a2a9933de5c538778c86
DanXxxxx <danxu0910@gmail.com>
2024-07-29 15:33:24 +0200

    add pop up for phd description

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let count = 0;
-const txt = "Hello, my name is Dan Xu. I am a creative researcher and maker/coder from China and based in the Netherlands.**I am particularly interested in the poetics and creative possibilities of human (and non-human) computer interaction**... as well as how such technologies mediate our embodied and social realities.**I like to blend insights and approaches from different disciplines to explore questions and phenomena that trigger my curiosity.**Besides research, I often make stuff with sound, text, and code.**>>>more>>>**&";
-const linkHTML = '<a href="https://danxxxu.github.io/relational-model/" target="_blank">PhD in interactive art @ Leiden University</a><br/><br/><a href="https://medium.com/@ProcessingOrg/meet-our-2024-processing-foundation-fellows-4b7f5ed5d104" target="_blank">(Re-)Imagining accessible web @ Processing Foundation</a><br/><br/><a href="https://digitalsocietyschool.org/digital-to-physical/" target="_blank">Digital transformation projects @ Digital Society School</a><br/><br/><a href="https://docs.google.com/document/d/1Jz5X7UPrQvlGTYuLwxc-Z1WEgav5zZhUWrxRNRbPN8w/view?usp=sharing" target="_blank">CV</a><br/><br/><a href="https://www.youtube.com/@danxu4968" target="_blank">Youtube</a><br/><br/><a href="https://www.instagram.com/danxx_910/" target="_blank">Instagram</a><br/><br/><a href="mailto: danxu0910@gmail.com">Email</a>';
+const txt = "Hello, my name is Dan Xu. I am a creative researcher and maker/coder from China based in the Netherlands.**I am particularly interested in the poetics and creative possibilities of human (and non-human) computer interaction**... as well as how such technologies mediate our embodied and social realities.**I like to blend insights and approaches from different disciplines to explore questions and phenomena that trigger my curiosity.**Besides research, I often make stuff with sound, text, and code.**>>>more>>>**&";
+const linkHTML = '<a a href="#" id="phd-link">PhD in interactive art @ Leiden University</a><br/><br/><a href="https://medium.com/@ProcessingOrg/meet-our-2024-processing-foundation-fellows-4b7f5ed5d104" target="_blank">(Re-)Imagining accessible web @ Processing Foundation</a><br/><br/><a href="https://digitalsocietyschool.org/digital-to-physical/" target="_blank">Digital transformation design @ Digital Society School</a><br/><br/><a href="https://docs.google.com/document/d/1Jz5X7UPrQvlGTYuLwxc-Z1WEgav5zZhUWrxRNRbPN8w/view?usp=sharing" target="_blank">CV</a><br/><br/><a href="https://www.youtube.com/@danxu4968" target="_blank">Some stuff I made</a><br/><br/><a href="https://www.instagram.com/danxx_910/" target="_blank">Instagram</a><br/><br/><a href="mailto: danxu0910@gmail.com">Email</a>';
 let speed = 0;
 
 function typeWriter() {
@@ -9,12 +9,27 @@ function typeWriter() {
             document.querySelector(".content").innerHTML += '<br/>'
         } else if (txt.charAt(count) == "&") {
             setTimeout(document.querySelector(".content").innerHTML += linkHTML, 50);
-        }
-        else{
+            loadLink();
+        } else {
             document.querySelector(".content").innerHTML += txt.charAt(count);
         }
         count++;
-        speed = Math.random() * 80;
+        speed = Math.random() * 50;
         setTimeout(typeWriter, speed);
     }
+}
+
+function loadLink() {
+    let phdLink = document.getElementById("phd-link");
+    let phdWindow = document.getElementById("phd-window");
+    let closeButton = document.getElementById("close");
+
+    phdLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        phdWindow.style.display = "block";
+    });
+
+    closeButton.addEventListener("click", function () {
+        phdWindow.style.display = "none";
+    });
 }
